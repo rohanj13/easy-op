c78c96d9a9457c8f6c953a946b52be70edd777e0
rohanj13 <rohan.australia@gmail.com>
2025-09-17 20:30:59 +1000

    moved rag pipeline code from jupyter to here

--- preopai-backend/preopai/settings/base.py
+++ preopai-backend/preopai/settings/base.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "drf_yasg",
+    "pinecone",
+    "openai",
     # local apps
     "hospitals",
     "patients",
